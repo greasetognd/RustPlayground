5bd2ed4c50609ab6956c3f2215c71160063b64d7
greasetognd <2233646@cegeplimoilou.ca>
2025-11-30 19:37:21 -0500

    ha

--- functions/read_number.rs
+++ functions/read_number.rs
@@ -5,3 +5,14 @@ pub fn read_number(prompt: &str) -> f64 {
     std::io::stdin().read_line(&mut input).unwrap();
     input.trim().parse().unwarp()
 }
+pub fn read_number(prompt: &str) -> f64 {
+    println!("{}", prompt);
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().parse().unwarp()
+    pub fn read_number(prompt: &str) -> f64 {
+        println!("{}", prompt);
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        input.trim().parse().unwarp()
+// lalalala
